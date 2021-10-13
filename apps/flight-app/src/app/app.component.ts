8b57c1bcb6426856bb405b612a0e29a75282c301
michael.zikes <michael.zikes@intauria.com>
2021-10-13 17:51:12 +0200

    Change Detection Counter

--- apps/flight-app/src/app/app.component.ts
+++ apps/flight-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, NgZone, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'flight-app',
@@ -6,4 +6,16 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('counter', {read: ElementRef}) counter: ElementRef | undefined;
+
+  constructor(private zone: NgZone) {}
+
+  cdCountUpdater() {
+    this.zone.runOutsideAngular(() => {
+      (this.counter as ElementRef) && (
+        (this.counter as ElementRef).nativeElement.textContent =
+          ++(this.counter as ElementRef).nativeElement.textContent
+      );
+    });
+  }
 }
