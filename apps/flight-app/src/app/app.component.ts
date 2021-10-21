66e12835da00c5e4997090158befba96e5faa53e
michael.zikes <michael.zikes@intauria.com>
2021-10-21 11:09:14 +0200

    RX Connector

--- apps/flight-app/src/app/app.component.ts
+++ apps/flight-app/src/app/app.component.ts
@@ -1,14 +1,33 @@
 import {Component, ElementRef, NgZone, ViewChild} from '@angular/core';
+import { timer, tap, take } from 'rxjs';
+import { RxConnector } from './rx-connector';
 
 @Component({
   selector: 'flight-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [RxConnector]
 })
 export class AppComponent {
   @ViewChild('counter', {read: ElementRef}) counter: ElementRef | undefined;
 
-  constructor(private zone: NgZone) {}
+  constructor(
+    private zone: NgZone,
+    private rx: RxConnector) {
+
+    this.rx.connect(
+      timer(0, 1000).pipe(take(10)),
+      {
+        next: value => console.log(value)
+      }
+    );
+    this.rx.connect(
+      timer(500, 1000).pipe(
+        take(10),
+        tap(value => console.log(value))
+      )
+    );
+  }
 
   cdCountUpdater() {
     this.zone.runOutsideAngular(() => {
